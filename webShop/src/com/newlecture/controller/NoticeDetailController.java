c67da3170369afa4a21fdbbb949381dc80dd669b
kangu <mo4mo3@daum.net>
2023-01-22 16:00:43 +0900

    JSTL: forTokens 첨부파일 목록 출력하기

--- webShop/src/com/newlecture/controller/NoticeDetailController.java
+++ webShop/src/com/newlecture/controller/NoticeDetailController.java
@@ -17,64 +17,54 @@ import javax.servlet.http.HttpServletResponse;
 import com.newlecture.web.entity.Notice;
 
 @WebServlet("/notice/detail")
-public class NoticeDetailController extends HttpServlet{
- @Override
-protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	 int id  = Integer.parseInt(request.getParameter("id"));
-	 
-	 String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
-	 String sql = "SELECT * FROM NOTICE WHERE ID=?";
-	 
-	 try {
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		 Connection con = DriverManager.getConnection(url, "scott", "tiger");
-		 PreparedStatement st = con.prepareStatement(sql);
-		 st.setInt(1, id);
-		 ResultSet rs = st.executeQuery();
-		 
-		 rs.next();
-		String title = rs.getString("TITLE");
-		String writerId =rs.getString("WRITER_ID") ;
-		Date regdate =rs.getDate("REGDATE") ;
-		String hit =rs.getString("HIT") ;
-		String files =rs.getString("FILES") ;
-		String content =rs.getString("CONTENT") ;
-		
-		Notice notice = new Notice(
-				id,
-				title, 
-				writerId,
-				regdate,
-				hit,
-				files,
-				content
-				);
-		
-		request.setAttribute("n", notice);
-		// request.setAttribute("title", title);
-		 //request.setAttribute("writerId", writerId);
-		 //request.setAttribute("regdate", regdate);
-		 //request.setAttribute("files", files);
-		 //request.setAttribute("content", title);
+public class NoticeDetailController extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
 
-		rs.close();
-		st.close();
-		con.close();
-	} catch (ClassNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+		String sql = "SELECT * FROM NOTICE WHERE ID=?";
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection(url, "scott", "tiger");
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setInt(1, id);
+			ResultSet rs = st.executeQuery();
+
+			rs.next();
+			String title = rs.getString("TITLE");
+			String writerId = rs.getString("WRITER_ID");
+			Date regdate = rs.getDate("REGDATE");
+			String hit = rs.getString("HIT");
+			String files = rs.getString("FILES");
+			String content = rs.getString("CONTENT");
+
+			Notice notice = new Notice(id, title, writerId, regdate, hit, files, content);
+
+			request.setAttribute("n", notice);
+			// request.setAttribute("title", title);
+			// request.setAttribute("writerId", writerId);
+			// request.setAttribute("regdate", regdate);
+			// request.setAttribute("files", files);
+			// request.setAttribute("content", title);
+
+			rs.close();
+			st.close();
+			con.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// redirect 서블릿 호출 다른페이지로 가고 싶을떄 게시글 등록후 목록페이지 전이
+
+		// forward 작업했떤 내용 이어받아 처리하고싶을때
+
+		request.getRequestDispatcher("WEB-INF/view/notice/detail.jsp").forward(request, response);
+
 	}
-	//redirect 서블릿 호출 다른페이지로 가고 싶을떄 게시글 등록후 목록페이지 전이
-
-	 //forward 작업했떤 내용 이어받아 처리하고싶을때
-	 
-	 
-	 request
-	 .getRequestDispatcher("WEB-INF/view/notice/detail.jsp")
-	 .forward(request, response);
- 
- }
 }
