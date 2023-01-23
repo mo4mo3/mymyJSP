c4089309ae46e38a4cdd1a4d40b527c6d0fcc4d0
kangu <mo4mo3@daum.net>
2023-01-23 21:00:48 +0900

    마지막 페이지 구하기

--- JDBprj/src/com/newlecture/app/console/NoticeConsole.java
+++ JDBprj/src/com/newlecture/app/console/NoticeConsole.java
@@ -11,72 +11,69 @@ public class NoticeConsole {
 
 	private NoticeService service;
 	private int page;
-	private int count;
-	
 
 	public NoticeConsole() {
-	
-	service = new NoticeService();
-	page =1 ;
-	count=0;
-	
-}	
-	
-	
-	
+
+		service = new NoticeService();
+		page = 1;
+
+	}
+
 	public void printNoticeList() throws ClassNotFoundException, SQLException {
-		
-	List<Notice>list = service.getList(page);
-	count =  service.getCount();
-			
-	System.out.println("---------------------------------");
-	System.out.printf("<공지사항> 총 %d 게시글\n", count);
-	System.out.println("---------------------------------");
-	
-	for(Notice n : list) {
-	System.out.printf("%d. %s / %s / %s \n", 
-					n.getId(), 
-					n.getTitle(), 
-					n.getWriterId(),
-					n.getRegDate());
-	}
-	System.out.println("---------------------------------");
-	System.out.printf("	%d/%d page\n", 1, 2);
 
+		List<Notice> list = service.getList(page);
+		int count = service.getCount();
+		int lastPage = count / 10; // 100 -> 10, 90 ->9, 93/10->9
+		lastPage = count % 10 > 0 ? lastPage + 1 : lastPage;
+
+		System.out.println("---------------------------------");
+		System.out.printf("<공지사항> 총 %d 게시글\n", count);
+		System.out.println("---------------------------------");
+
+		for (Notice n : list) {
+			System.out.printf("%d. %s / %s / %s \n", n.getId(), n.getTitle(), n.getWriterId(), n.getRegDate());
+		}
+		System.out.println("---------------------------------");
+		System.out.printf("	%d/%d page\n", page, lastPage);
 
 	}
 
-	
 	public int inputNoticeMenu() {
 		Scanner scan = new Scanner(System.in);
-		
+
 		System.out.printf("1.상세조회/2.이전/3.다음/4.글쓰기/5.종료>");
 		String menu_ = scan.nextLine();
 		int menu = Integer.parseInt(menu_);
 		return menu;
 	}
 
-
-
 	public void movePrevList() {
-		if(page ==	1 ) {
-			System.out.println("이전 페이지가 없습니다");
-		return;	
-	}
-		page--;
-		
-	}
+		if (page == 1) {
+			System.out.println("=================");
+			System.out.println("[이전 페이지가 없습니다]");
+			System.out.println("=================");
 
-
-
-	public void moveNextList() {
-		
-//		if(page ==	? ) {
-//			System.out.println("다음 페이지가 없습니다");
-//		return;	
-//		}
-		page++;		
-	
+			return;
 		}
+		page--;
+
+	}
+
+	public void moveNextList() throws ClassNotFoundException, SQLException {
+
+		int count = service.getCount();
+		int lastPage = count / 10; // 100 -> 10, 90 ->9, 93/10->9
+		lastPage = count % 10 > 0 ? lastPage + 1 : lastPage;
+
+		if (page == lastPage) {
+			System.out.println("=================");
+			System.out.println("[다음 페이지가 없습니다]");
+			System.out.println("=================");
+
+			return;
+		}
+		page++;
+
+	}
 
 }
