7cd1c6218e3d56e349d8a2dbace17204dbef2114
93019 <93019@DESKTOP-O627C3O>
2023-01-26 23:33:27 +0900

    JSP 2회 1-21

--- JSPPrj/src/com/newlecture/web/NoticeReg.java
+++ JSPPrj/src/com/newlecture/web/NoticeReg.java
@@ -1,6 +1,6 @@
 package com.newlecture.web;
 
-import java.io.IOException;
+import java.io.IOException; 
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
@@ -9,24 +9,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/hi")
-public class Nana extends HttpServlet {
+@WebServlet("/notice-reg")
+public class NoticeReg extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
-		
+/*		request.setCharacterEncoding("UTF-8");*/
 		PrintWriter out = response.getWriter();
 		
-		String cnt_ = request.getParameter("cnt");
-		
-		int cnt= 100;
-		if(cnt_ != null && !cnt_.equals(""))
-		cnt =Integer.parseInt(cnt_);
-				
-		for (int i = 0; i < cnt; i++)
-			out.println((i + 1) + "¾È³ç-<br>");
+		String title = request.getParameter("title");
+		String content = request.getParameter("content");
+
+		out.println(title);
+		out.println(content);
+	
 	}
 }
