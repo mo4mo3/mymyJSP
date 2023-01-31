c94d4d3f7f58887c62b6eb9aad6221207b86d1f9
mo4mo3 <mo4mo3@daum.net>
2023-01-31 19:13:59 +0900

    자세한 페이지 수정하기

--- JSPPrj/src/com/newlecture/web/controller/NoticeDetailController.java
+++ JSPPrj/src/com/newlecture/web/controller/NoticeDetailController.java
@@ -15,66 +15,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.newlecture.web.entity.Notice;
+import com.newlecture.web.service.NoticeService;
 
 @WebServlet("/notice/detail")
 public class NoticeDetailController extends HttpServlet {
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int id = Integer.parseInt(request.getParameter("id"));
-		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
-		String sql = "SELECT * FROM NOTICE WHERE ID=?";
-
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection con = DriverManager.getConnection(url, "NEWLEC", "1748");
-			PreparedStatement st = con.prepareStatement(sql);
-			st.setInt(1, id);
-			ResultSet rs = st.executeQuery();
-
-			rs.next();
-
-			String title = rs.getString("TITLE");
-			String writerId = rs.getString("WRITER_ID");
-			Date regdate = rs.getDate("REGDATE");
-			String hit = rs.getString("HIT");
-			String files = rs.getString("FILES");
-			String content = rs.getString("CONTENT");
-			
-			
-			Notice notice = new Notice(
-					id,
-					title,
-					writerId,
-					regdate,
-					hit,
-					files,
-					content
-					
-					);
-			
-			request.setAttribute("n", notice);
-/*
-			request.setAttribute("title", title);
-			request.setAttribute("writerId", writerId);
-			request.setAttribute("regdate", regdate);
-			request.setAttribute("hit", hit);
-			request.setAttribute("files", files);
-			request.setAttribute("content", content);*/
-
-			rs.close();
-			st.close();
-			con.close();
-
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+		 NoticeService service = new NoticeService();
+		 Notice notice = service.getNotice(id);
+		 request.setAttribute("n", notice);
 		// redirect
 		
 		
