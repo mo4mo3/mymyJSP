4e29d08b8f12066c7a1edaefdc2d470c19cb5840
mo4mo3 <mo4mo3@daum.net>
2023-02-05 20:28:09 +0900

    Spring tiles

--- webprj/src/main/java/com/newlecture/web/controller/IndexController.java
+++ webprj/src/main/java/com/newlecture/web/controller/IndexController.java
@@ -11,9 +11,9 @@ public class IndexController implements Controller {
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 	
-		ModelAndView mv = new ModelAndView();
+		ModelAndView mv = new ModelAndView("root.index");
 		mv.addObject("data", "Hello spring MVC~");
-		mv.setViewName("/WEB-INF/view/index.jsp");
+//		mv.setViewName("/WEB-INF/view/index.jsp");
 		return mv;
 	}
 
