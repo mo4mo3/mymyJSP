25b36544f9d55fbfa60464b063f37bc354681ce2
mo4mo3 <mo4mo3@daum.net>
2023-02-06 17:41:57 +0900

    @RestController와 한글출력 설정

---
+++ webprj/src/main/java/com/newlecture/web/controller/HomeController.java
@@ -0,0 +1,36 @@
+package com.newlecture.web.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+// http://...../WEBs
+
+@Controller
+@RequestMapping("/")
+public class HomeController{
+	
+	@RequestMapping("index")
+	@ResponseBody
+	public String index() {
+
+			return "Hello Index 1";
+	
+}
+
+}
+	
+/*	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	
+		ModelAndView mv = new ModelAndView("root.index");
+		mv.addObject("data", "Hello spring MVC~");
+//		mv.setViewName("/WEB-INF/view/index.jsp");
+		return mv;
+	}
+
+}*/
