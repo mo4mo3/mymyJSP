235b9bcf7dc57c8996d346e151802546106a7349
mo4mo3 <mo4mo3@daum.net>
2023-02-06 22:30:35 +0900

    POST 입력 #6(물리경로 얻기와 파일 저장하기)

--- webprj/src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
+++ webprj/src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
@@ -1,13 +1,23 @@
  package com.newlecture.web.controller.admin.board;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller("adminNoticeController")
 @RequestMapping("/admin/board/notice/")
 public class NoticeController {
 	//<bean name="adminnoticeController" class="com.newlecture.web.controller.admin.board.NoticeController" /> 
+		@Autowired
+		private ServletContext ctx;
 
 		@RequestMapping("list")
 		public String list() {
@@ -17,7 +27,30 @@ public class NoticeController {
 		}
 		@RequestMapping("reg")
 		@ResponseBody
-		public String reg(String title, String content, String category) {
+		public String reg(String title, String content, MultipartFile file, String category, String[] foods, String food, HttpServletRequest request) throws IllegalStateException, IOException {
+			
+			long size = file.getSize();
+			String fileName = file.getOriginalFilename();
+			System.out.printf("fileName:%s, fileSize:%d\n", fileName, size);
+			//ServletContext ctx = request.getServletContext();
+			String webPath = "/static/upload";
+			String realPath = ctx.getRealPath(webPath);
+			System.out.printf("realPath: %s\n", realPath);
+			//업로드 하기 위한 경로가 없을경우
+			File savePath = new File(realPath);
+			if(!savePath.exists())
+				 savePath.mkdir();
+			
+			realPath += File.separator +fileName;
+			File saveFile = new File(realPath);
+			file.transferTo(saveFile);
+			
+			System.out.println(category);
+			for(String f : foods)
+				System.out.println(f);
+			
+
+			
 			
 			return String.format("title:%s<br> content:%s<br>category:%s", title, content, category);
 		}
