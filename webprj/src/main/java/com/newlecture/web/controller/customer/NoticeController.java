27d728ae370cb92fc73b7f32bd21281965963a20
mo4mo3 <mo4mo3@daum.net>
2023-02-06 18:48:19 +0900

    @RequestParam Optional 속성

--- webprj/src/main/java/com/newlecture/web/controller/customer/NoticeController.java
+++ webprj/src/main/java/com/newlecture/web/controller/customer/NoticeController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.newlecture.web.entity.Notice;
 import com.newlecture.web.service.NoticeService;
@@ -22,9 +23,9 @@ public class NoticeController {
 	private NoticeService noticeService;
 	
 	@RequestMapping("list")
-		public String list(String p) throws ClassNotFoundException, SQLException {
+		public String list(@RequestParam(value="p",defaultValue="1") Integer page) throws ClassNotFoundException, SQLException {
 //			String p = request.getParameter("p");
-			System.out.println("page:"+p);
+			System.out.println("page:"+page);
 		
 			//List<Notice> list = noticeService.getList(1, "TITLE", "");
 		
